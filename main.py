64f9a76642560bd3e5bb446522681dbf0e43fd6b
mdenoux <matthieu.denoux@mines-paristech.fr>
2013-11-22 08:26:50 +0100

    Modification du main pour rajouter une liste de choix possibles

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ sys.path.append("recorder")
 
 from db import Db
 db = Db("db/")
+choice = 0
+while( not choice in range(1,3) ):
+    try:
+        choice = int(input("Que voulez-vous faire ?\n1-Enregistrer un élément\n2-Réaliser l'analyse d'un enregistrement\n"))
+    except NameError:
+        print "Ceci n'est pas un nombre !"
 
-from recorder import recorder
-recorder(db)
+if choice == 1:
+    from recorder import recorder
+    recorder(db)
+elif choice == 2:
+    pass
+else:
+    pass

a39d241ccae1733b1bf50c449067dfdeadf1ceff
sofiane87 <sofiane-mahiou@hotmail.fr>
2013-11-25 11:36:03 +0100

    correction fenetre_hann

--- src/fenetre_hann.py
+++ src/fenetre_hann.py
@@ -7,29 +7,33 @@ def hann_window_bis  (signal):
 	k =0
 	l =len(signal)
 	j =0
+	liste = []
 	while (k <l/(ecart_fenetre*RATE) and j<((2*l)-(ecart_fenetre*RATE))):
-		for i in range(int(temps_fenetre*RATE)):
-			signal[k*int(ecart_fenetre*RATE)+i]*= 0.5-0.5*np.cos(2*(np.pi)*(float(i)/(RATE*temps_fenetre)))
+		L =[]
+		for i in range(int(temps_fenetre*RATE)+1):
+			L.append(signal[k*int(ecart_fenetre*RATE)+i]*(0.5-0.5*np.cos(2*(np.pi)*(float(i)/(RATE*temps_fenetre)))))
 			j += 1
-			if (j == l):
-				break
+		liste.append(L)
 		k+=1
-	return signal		
+	return 	liste
 		
 def hann_window(signal):
 	l =len(signal)
 	k =0
+	liste = []
 	while (k <l/(ecart_fenetre*RATE)):
-		for i in range(int(temps_fenetre*RATE)):
+		L =[]
+		for i in range(int(temps_fenetre*RATE)+1):
 			try:
-				signal[k*int(ecart_fenetre*RATE)+i]*= 0.5-0.5*np.cos(2*(np.pi)*(float(i)/(RATE*temps_fenetre)))
+				L.append(signal[k*int(ecart_fenetre*RATE)+i]*(0.5-0.5*np.cos(2*(np.pi)*(float(i)/(RATE*temps_fenetre)))))
 			except IndexError:
 				break 
+		liste.append(L)
 		k+=1
-	return signal			
+	return liste	
 
 if __name__ == "__main__":
 #test de verification 	
-	z = [250*i/100000 for i in range (88200)]
+	z = [250.*i/100000 for i in range (88200)]
 	
-	print(hann_window(z)==hann_window_bis(z))
+	print(len(hann_window(z)[15]))

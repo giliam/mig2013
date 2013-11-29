bd0213f6ae895e07198464bb3ba0334671510c6e
Maxime Ernoult <max@wumzi.info>
2013-11-29 11:10:09 +0100

    Ajout d'un module de conversion d'ogg en wav
    ts commit f3e304967d410c116b6de6535cc0950ec7ae87c0.

---
+++ speechapp/speechserver/audioRecorder.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python2
+# -*- coding; utf-8 -*-
+
+"""Module de conversion de fichiers ogg en wav
+Il faut avoir sox et soundrecorder sur le serveur"""
+
+import os
+from random import randint
+
+TMP_DIR = "tmp/"
+
+def path_orig_ogg(id):
+    return TMP_DIR + "orig_" + str(id) + ".ogg"
+
+
+def path_mid_wave(id):
+    return TMP_DIR + "orig_" + str(id) + ".wav"
+
+
+def path_final_wave(id):
+    return TMP_DIR + "wave_final_" + str(id) + ".wav"
+
+
+def rm_multi(*files):
+    """Remove multiple files"""
+    for path in files:
+        os.remove(path)
+
+def convert_ogg_to_wave(oggBlob):
+    """Converti le blob ogg en blob wav"""
+    id = randint(1, 1000)
+    while os.access(path_orig_ogg(id), os.W_OK):
+        id = randint(1, 1000)
+
+    with open(path_orig_ogg(id), 'w') as origoggfile:
+         origoggfile.write(oggBlob)
+         origoggfile.flush()
+         os.fsync(origoggfile)
+         origoggfile.close()
+
+    #Now convert the file
+
+    os.system('soundconverter -b -m audio/x-wav -s .wav "%s"' %  path_orig_ogg(id))
+    #Resample to 44.1kHz
+    os.system('sox -r 44.1k -e signed -c 2 -b 16 %s %s' % (path_mid_wave(id), path_final_wave(id)))
+
+    #And read the oggBlob
+
+    with open(path_final_wave(id), 'r') as finalwavefile:
+        waveBlob =  finalwavefile.read()
+        finalwavefile.close()
+    
+    #Remove the files
+    rm_multi(path_orig_ogg(id), path_mid_wave(id), path_final_wave(id))
+    return waveBlob
+
+if __name__ == '__main__':
+    f = open('so.oga', 'r')
+    
+    print(convert_ogg_to_wave(f.read()))
+
+    f.close()    
